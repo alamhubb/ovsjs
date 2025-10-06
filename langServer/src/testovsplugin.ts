140917062783cf10be3ad6d4742958292cfa4f26
alamhubb <alamhubb@163.com>
2025-10-06 23:31:11 +0800

    成功支持 if 模块

--- langServer/src/testovsplugin.ts
+++ langServer/src/testovsplugin.ts
@@ -1,10 +1,17 @@
 import {vitePluginOvsTransform} from "ovsjs/src"
 
-const code = `div{ 
-if(true) {
-  123
+const code = `
+export const test = {
+  render() {
+    return div { 
+      const abc = true
+      if (abc) {
+        123
+      }
+    }
+  }
 }
-}`
+`
 
 const res = vitePluginOvsTransform(code)
 console.log('code:', res.code)
