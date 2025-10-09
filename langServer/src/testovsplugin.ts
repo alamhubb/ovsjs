935394d382dfdcaa0a99b0ae5a03d231747288cd
alamhubb <alamhubb@163.com>
2025-10-09 14:35:54 +0800

    成功支持 if 模块

--- langServer/src/testovsplugin.ts
+++ langServer/src/testovsplugin.ts
@@ -3,10 +3,15 @@ import {vitePluginOvsTransform} from "ovsjs/src"
 
 Error.stackTraceLimit = 50
 
-const code = `
-object a {}
+const code = `  div { 
+      const abc = true
+      if (abc) {
+        123
+      }
+}
 `
 
 const res = vitePluginOvsTransform(code)
 console.log('code:', res.code)
+
 
