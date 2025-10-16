1048afbc6b40a29d7a1e3f5771e716e7a64bb555
alamhubb <alamhubb@163.com>
2025-10-16 16:13:38 +0800

    支持缓存

--- subhuti/src/parser/SubhutiTokenConsumer.ts
+++ subhuti/src/parser/SubhutiTokenConsumer.ts
@@ -11,16 +11,4 @@ export default class SubhutiTokenConsumer {
     consume(token: SubhutiCreateToken) {
         this.instance.consume(token)
     }
-
-    or(subhutiParserOrs: SubhutiParserOr[]) {
-        // 为tokenConsumer的or调用添加特殊标记到调用栈
-        // 避免与Parser规则的Or缓存冲突
-        const stackMarker = 'TokenConsumer_or'
-        ;(this.instance as any).ruleExecErrorStack.push(stackMarker)
-        try {
-            this.instance.Or(subhutiParserOrs)
-        } finally {
-            ;(this.instance as any).ruleExecErrorStack.pop()
-        }
-    }
 }
