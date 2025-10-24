13b8bae3d8e5a43b913ba7d8bfa1aa6432998f94
alamhubb <alamhubb@163.com>
2025-10-24 15:39:04 +0800

    调整ovs语法，#{} 内代码不渲染

--- ovs/example/src/main.ts
+++ ovs/example/src/main.ts
@@ -1,21 +1,8 @@
 import { createApp, h } from 'vue'
 import DefaultViews from "@/views/hello.ovs"
-import { toVnode } from './utils/ReactiveVNode'
 
 createApp({
   setup() {
-    return () => {
-      // DefaultViews 是 ReactiveVNode 数组，需要转换成 VNode
-      if (Array.isArray(DefaultViews)) {
-        return DefaultViews.map(item => {
-          // 如果是 ReactiveVNodeApi，调用 toVnode()
-          if (item && typeof item === 'object' && 'toVnode' in item) {
-            return item.toVnode()
-          }
-          return item
-        })
-      }
-      return DefaultViews
-    }
+    return () => DefaultViews.map(v => v.toVnode())
   }
 }).mount('#app')
