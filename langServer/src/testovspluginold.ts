084561ee1ee8223183e3895e8a0c9a813dfe9067
alamhubb <alamhubb@163.com>
2025-10-31 20:42:19 +0800

    回复osserver代码

--- langServer/src/testovspluginold.ts
+++ langServer/src/testovspluginold.ts
@@ -1,6 +1,10 @@
-import {ovsTransform} from "ovsjs/src";
+import {ovsTransform, vitePluginOvsTransform} from "ovsjs/src";
 
-const code = `createComponentVNode(div,{},[createComponentVNode(...)])
+const code = `
+export default (function(){
+  const children = [];
+  children.push(createComponentVNode(div,{},[createComponentVNode(h1,{},[appName,]),createComponentVNode(p,{},[version,]),]))
+  return children})()
 `
 
 const res = ovsTransform(code)
