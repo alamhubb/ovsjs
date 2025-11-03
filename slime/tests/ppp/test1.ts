f4d9bc0f65a5ccc475a5fd77ad3337edefa29bcd
alamhubb <alamhubb@163.com>
2025-11-03 17:38:39 +0800

    修复or和many

--- slime/tests/ppp/test1.ts
+++ slime/tests/ppp/test1.ts
@@ -3,41 +3,8 @@ import JsonUtil from "subhuti/src/utils/JsonUtil";
 import {traverseClearLoc, traverseClearTokens} from "../utils/parserTestUtils";
 import {LogUtil} from "../../src/logutil";
 import {es2020Tokens} from "../../packages/slime-parser/src/language/es2020/Es2020Tokens";
-import Es2020Parser from "../../packages/slime-parser/src/language/es2020/Es2020Parser";
-import Es6Parser from "slime-parser/src/language/es2015/Es6Parser";
 
-const code = `
-const [first, ...rest] = arr
-const [a, b, ...others] = list
-
-// ✅ 测试2：只有rest，没有前置元素    BindingRestElement -> ... BindingIdentifier (sole rest)
-const [...all] = array
-const [...items] = data
-
-// ✅ 测试3：函数参数rest    BindingRestElement -> ... BindingIdentifier (function params)
-function test(first, ...args) {
-    return args
-}
-
-// ✅ 测试4：函数参数只有rest
-function collect(...items) {
-    return items
-}
-
-// ✅ 测试5：对象rest（ES2018 - 可能不支持）
-const {name, ...otherProps} = obj
-
-// ✅ 测试6：嵌套解构中的rest
-const [first, ...[second, third]] = arr
-
-// ✅ 测试7：rest配合默认值
-function sum(initial = 0, ...nums) {
-    return nums.reduce((a, b) => a + b, initial)
-}
-
-// ✅ 测试8：多个函数使用rest
-const fn1 = (...args) => args.length
-const fn2 = (a, b, ...rest) => rest
+const code = `const [first, ...[second, third]] = arr
 `
 
 const lexer = new SubhutiLexer(es2020Tokens)
