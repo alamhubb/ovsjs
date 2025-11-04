c103c99401da86c0777e359824555e479d28939a
alamhubb <alamhubb@163.com>
2025-11-05 00:43:48 +0800

    一会恢复这个版本，现在开始开发 2025parser

--- ovs/example/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import DefaultViews from "@/views/hello.ovs"
-
-createApp({
-  setup() {
-    return () => DefaultViews.map(v => v.toVnode())
-  }
-}).mount('#app')
--- ovs/src/model/OvsVueRenderAst.ts
+++
@@ -1,8 +0,0 @@
-import SubhutiCst from "subhuti/src/struct/SubhutiCst.ts";
-
-export default class OvsVueRenderAst {
-    slotName: string
-    component: SubhutiCst
-    props: SubhutiCst[]
-    OvsChildList: SubhutiCst
-}
