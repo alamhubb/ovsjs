f6b47e03c6e5e004ff8e4a19f1f3d9caca1bdf06
alamhubb <alamhubb@163.com>
2025-11-30 00:01:10 +0800

    删除返回cst相关内容，只根据解析状态判断

--- slime/packages/slime-ast/src/errorParserTest.ts
+++ slime/packages/slime-ast/src/errorParserTest.ts
@@ -1,6 +1,7 @@
 import SubhutiLexer from "subhuti/src/SubhutiLexer.ts";
 import {es2025Tokens} from "slime-parser/src/language/es2025/Es2025Tokens.ts";
 import Es2025Parser from "slime-parser/src/language/es2025/Es2025Parser.ts";
+import slimeCstToAstUtil from "slime-parser/src/language/SlimeCstToAstUtil.ts";
 
 // 测试：第一个语句不完整，第二个语句完整
 const code = 'let a = {'
@@ -16,5 +17,6 @@ const parser = new Es2025Parser(tokens)
 parser.enableErrorRecovery()
 
 const res = parser.Program()
+
 
 console.log('Result:', JSON.stringify(res, null, 2))
