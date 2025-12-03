d9b5cce64e5b4c8fdbf59c5cb7177cb64405b905
alamhubb <alamhubb@163.com>
2025-12-03 08:30:21 +0800

    修复stage1,2,3的问题

--- ovs/src/parser/OvsConsumer.ts
+++ ovs/src/parser/OvsConsumer.ts
@@ -1,24 +1,26 @@
 import { createKeywordToken, createRegToken } from 'subhuti/src/struct/SubhutiCreateToken.ts'
-import Es6TokenConsumer, {Es6TokenName, es6TokensObj} from "slime-parser/src/language/es2015/Es6Tokens.ts";
+import {SlimeTokensObj} from "slime-parser/src/language/es2025/SlimeTokens.ts";
+import {SlimeTokenType} from "slime-token/src/SlimeTokenType.ts";
+import SlimeTokenConsumer from "slime-parser/src/language/es2025/SlimeTokenConsumer.ts";
 
 export const ovsTokenName = {
-  ...Es6TokenName,
+  ...SlimeTokenType,
   OvsViewToken: "OvsViewToken",
   Hash: "Hash"
 }
 export const ovsTokensObj = {
-  ...es6TokensObj,
+  ...SlimeTokensObj,
   OvsViewToken: createKeywordToken(ovsTokenName.OvsViewToken, "ovsView"),
   Hash: createRegToken(ovsTokenName.Hash, /^#/)
 }
 export const ovs6Tokens = Object.values(ovsTokensObj)
 
-export default class OvsTokenConsumer extends Es6TokenConsumer {
+export default class OvsTokenConsumer extends SlimeTokenConsumer {
   OvsViewToken() {
-    return this.consume(ovsTokensObj.OvsViewToken)
+    return this.consume(ovsTokenName.OvsViewToken)
   }
 
   Hash() {
-    return this.consume(ovsTokensObj.Hash)
+    return this.consume(ovsTokenName.Hash)
   }
 }
