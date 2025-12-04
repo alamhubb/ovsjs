a8824cc60eb72db097c672499fa904796a7709e2
alamhubb <alamhubb@163.com>
2025-12-04 20:14:09 +0800

    修复类型导入的错误

--- ovs/tests/utils/test-stage1.ts
+++ ovs/tests/utils/test-stage1.ts
@@ -7,9 +7,7 @@
  *   npx tsx ovs/tests/utils/test-stage1.ts 10           # 从第10个开始
  *   npx tsx ovs/tests/utils/test-stage1.ts 10 -s        # 从第10个开始，遇错停止
  */
-import { TestContext, TestResult, parseToCst, runTests } from 'slime/tests/utils/test-framework'
-import OvsParser from '../../src/parser/OvsParser'
-import { OvsCstToSlimeAst } from '../../src/factory/OvsCstToSlimeAstUtil'
+import { TestContext, TestResult, parseToCst, runTests } from 'slimejs/tests/utils/test-framework.ts'
 
 // 阶段1测试逻辑：解析代码生成 CST
 function testStage1(ctx: TestContext): TestResult {
