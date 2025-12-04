02459170749d1f7b7232e4c0ac584dc1c53cd3f7
alamhubb <alamhubb@163.com>
2025-12-04 14:28:19 +0800

    修改项目名

--- packages/language-server/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './lib/types';
-export * from './protocol';
-
-// only export types of depend packages
-export * from '@volar/language-core/lib/types';
-export * from '@volar/language-service/lib/types';
-export * from 'vscode-languageserver';
--- packages/language-service/lib/utils/cancellation.ts
+++
@@ -1,6 +0,0 @@
-import type * as vscode from 'vscode-languageserver-protocol';
-
-export const NoneCancellationToken: vscode.CancellationToken = {
-	isCancellationRequested: false,
-	onCancellationRequested: () => ({ dispose: () => {} }),
-};
