d195498d848653df7291b23325e362ecda85a703
alamhubb <alamhubb@163.com>
2025-12-06 02:10:47 +0800

    增加代码格式化换行相关功能

--- ovs/src/model/ovsRenderDomClassDeclaration.ts
+++
@@ -1,6 +0,0 @@
-import SubhutiCst from 'subhuti/src/struct/SubhutiCst.ts.'
-
-export default class OvsRenderDomClassDeclaration {
-  className: SubhutiCst
-  props: SubhutiCst
-}
--- ovs/src/view/OvsView.ts
+++
@@ -1,3 +0,0 @@
-export default class OvsView {
-  //编译的时候实现，children 的顺序
-}
