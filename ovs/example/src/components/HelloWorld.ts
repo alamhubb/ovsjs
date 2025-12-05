daf4a67ded1bfacf539b8bba797e0b9467530a77
alamhubb <alamhubb@163.com>
2025-12-05 20:39:38 +0800

    增加代码格式化换行相关功能

--- ovs/example/src/components/HelloWorld.ts
+++ ovs/example/src/components/HelloWorld.ts
@@ -1,13 +1,16 @@
-import {ref, h} from 'vue';
+import { ref, h, defineComponent } from 'vue'
 
-export default function HelloWorld() {
-    const a1 = ref(0)
-    setInterval(() => {
-        a1.value++
-    }, 1000)
-    return () => h('div', {
-        onClick() {
-            a1.value = 0
-        }
-    }, [a1.value])
-}
+export default defineComponent({
+    setup() {
+        const a1 = ref(0)
+
+        setInterval(() => {
+            a1.value++
+        }, 1000)
+
+        // setup 返回函数时，Vue 认为这是 render 函数
+        return () => h('div', {
+            onClick() { a1.value = 0 }
+        }, [a1.value])
+    }
+})
