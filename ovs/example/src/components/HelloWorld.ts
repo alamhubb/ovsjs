0f295c4a877d54ee8c4a8eafc8777ef103669fb5
alamhubb <alamhubb@163.com>
2025-12-05 21:01:23 +0800

    增加代码格式化换行相关功能

--- ovs/example/src/components/HelloWorld.ts
+++ ovs/example/src/components/HelloWorld.ts
@@ -1,16 +1,15 @@
-import { ref, h, defineComponent } from 'vue'
+import {h, ref, defineComponent} from 'vue'
 
-export default defineComponent({
-    setup() {
-        const a1 = ref(0)
+export default defineComponent((props) => {
+    const a1 = ref(0)
 
-        setInterval(() => {
-            a1.value++
-        }, 1000)
-
-        // setup 返回函数时，Vue 认为这是 render 函数
-        return () => h('div', {
-            onClick() { a1.value = 0 }
-        }, [a1.value])
+    // 定时器只创建一次
+    setInterval(() => {
+        a1.value++
+    }, 1000)
+    return () => {
+        // 这里只读，不改！
+        // 而且不能写 a1.value++，只能显示 a1.value
+        return h('div', [a1.value])
     }
 })
