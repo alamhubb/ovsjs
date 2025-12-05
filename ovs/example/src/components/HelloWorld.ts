c27b6e528541fc9fb2adac124c5108c61514a294
alamhubb <alamhubb@163.com>
2025-12-05 20:17:05 +0800

    增加代码格式化换行相关功能

--- ovs/example/src/components/HelloWorld.ts
+++ ovs/example/src/components/HelloWorld.ts
@@ -1,4 +1,4 @@
-import { ref,h } from 'vue';
+import {ref, h, unref} from 'vue';
 
 // 你想要的 IIFE 风格，但返回一个组件定义
 export default (function () {
@@ -10,12 +10,20 @@ export default (function () {
 
     // 返回 Options 组件，Vue 负责响应式更新
     return {
-        render() {
+        toVnode() {
+            // 直接在 toVnode 里访问闭包中的 ref
             return h('div', {
                 onClick() {
                     a1.value = 0
                 }
-            }, [a1.value])  // 这里用 .value 是 OK 的！
+            }, [a1.value])
         }
     }
 })()
+
+// return {
+//     toVnode() {
+//         // 直接在 toVnode 里访问闭包中的 ref
+//         return h('div', { onClick: () => a1.value = 0 }, [unref(a1)])
+//     }
+// }
