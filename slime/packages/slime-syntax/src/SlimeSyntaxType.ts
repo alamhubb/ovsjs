8d91301808916a0d76da721149336e561fa12092
alamhubb <alamhubb@163.com>
2025-12-05 11:41:02 +0800

    支持启动

--- slime/packages/slime-syntax/src/SlimeSyntaxType.ts
+++
@@ -1,8 +0,0 @@
-export enum SlimeSyntaxType {
-    VariableDeclarator = 'VariableDeclarator',
-    NumericLiteral = 'NumberLiteral',
-    StringLiteral = 'StringLiteral',
-    BooleanLiteral = 'BooleanLiteral',
-    NullLiteral = 'NullLiteral',
-    Initializer = 'Initializer'
-}
