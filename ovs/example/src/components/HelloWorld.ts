cfa06e2e7473ee350fcc9e6b649ac5a31c5d1cd1
alamhubb <alamhubb@163.com>
2025-12-05 21:29:57 +0800

    增加代码格式化换行相关功能

--- ovs/example/src/components/HelloWorld.ts
+++ ovs/example/src/components/HelloWorld.ts
@@ -10,6 +10,6 @@ export default defineComponent((props) => {
     return () => {
         // 这里只读，不改！
         // 而且不能写 a1.value++，只能显示 a1.value
-        return h('div', [a1.value])
+        return h('div', {onClick: () => a1.value = 0}, [a1.value])
     }
 })
